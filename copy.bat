2862c807930dc31c84dd4a26342f25afaedc8328
Kazuhiro KANEMATSU <kanematsu.kazuhiro@po.ntt-tx.co.jp>
2018-01-17 10:58:58 +0900

    Add Batch File.

---
+++ copy.bat
@@ -0,0 +1,3 @@
+xcopy ..\quilt\crypto-conditions\target\apidocs crypto-conditions\target\apidocs /D /S /R /Y /I /K
+xcopy ..\quilt\ilp-annotations\target\apidocs ilp-annotations\target\apidocs /D /S /R /Y /I /K
+xcopy ..\quilt\ilp-core\target\apidocs ilp-core\target\apidocs /D /S /R /Y /I /K
